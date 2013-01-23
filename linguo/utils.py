d87a13fc0c6e35f477bf44f0546ac2cb1d3e9b60
Zach Mathew <zmathew.to@gmail.com>
2013-01-22 22:53:39 -0500

    Fixes typo in doc string.

--- linguo/utils.py
+++ linguo/utils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def get_real_field_name(field_name, language):
     """
-    Returns the field that stores the value of the given translatable
+    Returns the field that stores the value of the given translation
     in the specified language.
     """
     if language == settings.LANGUAGES[0][0]:
