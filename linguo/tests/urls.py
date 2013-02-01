a159d8bd3580c5b08368f534e687907ad365b33a
Zach Mathew <zmathew.to@gmail.com>
2013-01-31 23:31:14 -0500

    Attempts old style url.conf imports if new style fails.

--- linguo/tests/urls.py
+++ linguo/tests/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url, include
+except ImportError:  # For backwards compatibility with Django < 1.4
+    from django.conf.urls.defaults import patterns, url, include
+
 from django.contrib import admin
 
 
