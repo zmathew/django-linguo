69575694012cd161afcd8fdd9bfdd093df301b06
Luan Pablo <luanpab@gmail.com>
2013-06-24 11:49:28 -0300

    Adding changes to work with languages from diferent locales
    as example, if you want to work with pt and pt-br.

--- linguo/utils.py
+++ linguo/utils.py
@@ -11,15 +11,15 @@ def get_real_field_name(field_name, language):
     if lang_code == get_normalized_language(settings.LANGUAGES[0][0]):
         return field_name
     else:
-        return '%s_%s' % (field_name, language)
+        return '%s_%s' % (field_name, lang_code)
 
 
 def get_normalized_language(language_code):
     """
-    Returns the actual language extracted from the given language code
-    (ie. locale stripped off). For example, 'en-us' becomes 'en'.
+    Return normalized language (ie. just like locale). For example, 'en-us'
+    becomes 'en_us'.
     """
-    return language_code.split('-')[0]
+    return language_code.replace('-', '_')
 
 
 def get_current_language():
