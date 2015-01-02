fd6d65d67903e2fdef172c379b554abb38ad27cd
Zach Mathew <zmathew.to@gmail.com>
2015-01-02 17:25:19 -0500

    Bumps version to 1.4.0

--- linguo/__init__.py
+++ linguo/__init__.py
@@ -1,5 +1,5 @@
 """Linguo aims to make model translation easy and is designed to let you use the built-in Django features (Query API, Model Forms, Admin, etc) as intended."""
 
-VERSION = (1, 3, 2)
+VERSION = (1, 4, 0)
 
 __version__ = '.'.join(map(str, VERSION))
